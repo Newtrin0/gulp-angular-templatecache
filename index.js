f9916e595d9cdf37f1e98714c0c76e8d5b3e2809
Neutrino <Edmundas.Miseikis@gmail.com>
2014-03-20 14:53:54 +0200

    Remove file.cwd from file.path by default
    Remove current directory from the file path if base parameter was not provided

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function templateCache(root, base) {
 
 	return es.map(function(file, cb) {
 		var template = '$templateCache.put("<%= url %>","<%= contents %>");';
-		var url = path.join(root, file.path.replace(base || file.base, ''));
+		var url = path.join(root, file.path.replace(base || file.cwd + '/' + file.base, ''));
 
 		if (process.platform === 'win32') {
 			url = url.replace(/\\/g, '/');
